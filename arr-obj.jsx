fc854e4fee8db1c83157e8d5e6c63a0791253eda
Gajjar Dhriti <dhriti.g1012@gmail.com>
2026-04-24 01:13:27 +0530

    Add files via upload

---
+++ arr-obj.jsx
@@ -0,0 +1,33 @@
+// arr-obj.jsx
+const products = [
+  {
+    id: 1,
+    name: "Smartphone",
+    price: "₹15,999",
+    description: "6.5-inch display, 128GB storage, 48MP camera",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRUjDc1i0XeOEkeTK0ScNkaE1Xnxwms3HgIQ&s"
+  },
+  {
+    id: 2,
+    name: "Laptop",
+    price: "₹49,999",
+    description: "Intel i5, 8GB RAM, 512GB SSD",
+    image: "https://i.dell.com/is/image/DellContent/content/dam/images/products/laptops-and-2-in-1s/dell-laptop/db14250-non-touch/dell-db14250nt-laptop-c-22040rf115-bl-fpr.psd?wid=1200&hei=630&fmt=png-alpha"
+  },
+  {
+    id: 3,
+    name: "Headphones",
+    price: "₹2,499",
+    description: "Wireless, Noise Cancelling, 20hr battery",
+    image: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTkscnQEhjBh2tHA4d1GtiNA8_cJJl3mHp9z0QEqnV6QeWaeZMvl6gum82LjSF6zHdXycv11gOxgm3qL3tix6a0NwhQqhH0V3jq7at6anQzrmjWQ6Vy-IZaRwdGOy9cii4ZfokDCA&usqp=CAc"
+  },
+  {
+    id: 4,
+    name: "Smartwatch",
+    price: "₹5,999",
+    description: "Heart rate monitor, GPS, Waterproof",
+    image: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcR_7dSvm-LH-zkL-zMm7XMkWFmlWvUqeRYYhwH6awj2VS8ly9EpvRgpE16QLXuRkRVH79F9UA0vkqOkGNz7Gk8e7FycbezBiCSv1wSt8fsGHveCGE_os-0cJCQ7v1OpGPo6q-RgWA&usqp=CAc"
+  }
+];
+
+export default products;   // <-- EXPORT HERE
